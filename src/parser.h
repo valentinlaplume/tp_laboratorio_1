8c06bc3fc247d2243cddfa73ecc44b5685a6de08
valentinlaplume <78327319+valentinlaplume@users.noreply.github.com>
2021-06-26 12:19:18 -0300

    Delete src directory

--- src/parser.h
+++
@@ -1,3 +0,0 @@
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeUltimoIdFromText(FILE* pFile, LinkedList* pArrayListEmployee);
